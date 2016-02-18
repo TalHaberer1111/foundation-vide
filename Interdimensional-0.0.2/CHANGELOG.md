6db56aeff0465a0950dec2e4521a7504bec118c6
tal haberer and wren withers <student@epicodus.com>
2016-02-18 15:07:49 -0800

    add vibe video and foundation styles and grid

--- Interdimensional-0.0.2/CHANGELOG.md
+++
@@ -1,4 +0,0 @@
-### 0.0.2
-* Fixed docs.
-* Fix custom controls.
-* Updated dependencies.
---
+++ js/scripts.js
@@ -0,0 +1,22 @@
+// $('#myBlock').vide('../video/star.mp4');
+// $('#myBlock').vide('../video/star.mp4', {
+//
+// });
+// $('#myBlock').vide({
+//   mp4: ../video/star.mp4,
+//   poster: ../video/video.gif
+//
+// },
+//
+// });
+// $('#myBlock').vide('extended path as a string', 'options as a string');
+//
+// // Get instance of the plugin
+// var instance = $('#myBlock').data('vide');
+//
+// // Get video element of the background. Do what you want.
+// instance.getVideoObject();
+//
+// // Resize video background.
+// // It calls automatically, if window resize (or element, if you will use something like https://github.com/cowboy/jquery-resize).
+// instance.resize();
